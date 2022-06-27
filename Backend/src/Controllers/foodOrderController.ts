5e7d4697707b406b56a8c0995a663959b15f3abb
SarangTandel5112 <sarangtandel5112@gmail.com>
2022-06-27 17:21:19 +0530

    order

--- Backend/src/Controllers/foodOrderController.ts
+++ Backend/src/Controllers/foodOrderController.ts
@@ -1,27 +1,27 @@
-import {Request, Response } from "express"; 
+import { Request, Response } from "express";
 import FoodOrders from "../Model/user";
 class FoodOrder {
-public orderFood = async (req:Request,res:Response)=>{
-    const {name , item} =req.body;
-    if(!name){
-        return res.status(422).json({status:false,data:"Please provide name"})
-    }
-    if(!item){
-        return res.status(422).json({status:false,data:"Please provide item"})
-    }
-    const order = new FoodOrders({
-        name:name,
-        item:item
-    })
-    await order.save();
-    return res.status(201).json({status:true,data:'Your Order has been placed successfully'})
-
-    
-
-};
+    public orderFood = async (req: Request, res: Response) => {
+        const { name, item } = req.body;
+        if (!name) {
+            return res.status(422).json({ status: false, data: "Please provide name" })
+        }
+        if (!item) {
+            return res.status(422).json({ status: false, data: "Please provide item" })
+        }
+        const order = new FoodOrders({
+            name: name,
+            item: item
+        })
+        await order.save();
+        return res.status(201).json({ status: true, data: 'Your Order has been placed successfully' })
 
 
-   
+
+    };
+
+
+
 }
 
 
--- frontend/src/components/Order.jsx
+++ frontend/src/components/Order.jsx
@@ -1,9 +1,90 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Order() {
-  return (
-    <div>Order</div>
-  )
+
+    const [data, setdata] = useState({
+        name: "",
+        item: ""
+    })
+
+    function handelChange(event) {
+        event.preventDefault();
+        alert(event.target.value);
+        setdata({ ...data, [event.target.name]: event.target.value })
+    }
+    function submitData() {
+        console.log(data);
+    }
+
+    return (
+        <div>
+            <div class="login-box">
+                <h2>Welcome Foodies</h2>
+                <form>
+                    <div class="user-box">
+                        <input type="text" name="name" required="" value={data.name} onChange={handelChange} />
+                        <label>Foodie's Name :</label>
+                    </div>
+                    {/* <div class="user-box">
+                        <input type="password" name="" required="" />
+                        <label>Password</label>
+                    </div> */}
+                    <div className='row1'>
+                        <button className='itembtn' type='button' name='item' value="item1" onChange={handelChange}>
+                            <a href="#">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                Item1
+                            </a>
+                        </button>
+                        <button className='itembtn' name='item' value="item2" onChange={handelChange}>
+                            <a href="#">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                Item2
+                            </a>
+                        </button>
+                    </div>
+                    <br></br>
+                    <div className='row2'>
+                        <button className='itembtn' name='item' value="item3" onChange={handelChange}>
+                            <a href="#">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                Item3
+                            </a>
+                        </button>
+                        <button className='itembtn' name='item' value="item4" onChange={handelChange}>
+                            <a href="#">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                Item4
+                            </a>
+                        </button>
+                    </div>
+                    <div className='submitbtn' onClick={submitData}>
+                        <a href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            Place Order
+                        </a>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Order
